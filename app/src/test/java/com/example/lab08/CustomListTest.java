87aa32516e062c08faaae1c2982a90211a3e8374
Swayam1129 <swayam2@ualberta.ca>
2025-10-28 13:47:51 -0600

    failes test added

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,18 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    @Test
+    public void testHasCity() {
+        CustomList cityList = new CustomList();
+        City city = new City("Edmonton", "AB");
+        cityList.addCity(city);
+
+        // Expect true since the city was added
+        assertTrue(cityList.hasCity(city));
+
+        // Expect false for a city not in the list
+        City notInList = new City("Calgary", "AB");
+        assertFalse(cityList.hasCity(notInList));
+    }
 
 }
